59c3092277d471073b74f440b2b29cd7845256ea
Wang <1911397210@qq.com>
2019-02-02 20:15:20 +0800

    我的之优惠卷

--- src/router/router.js
+++ src/router/router.js
@@ -6,17 +6,33 @@ const msite = r => require.ensure([], () => r(require('../page/msite/msite')), '
 const login = r => require.ensure([], () => r(require('../page/login/login')), 'login')
 const shop = r => require.ensure([], () => r(require('../page/shop/shop')), 'shop')
 const profile = r => require.ensure([], () => r(require('../page/profile/profile')), 'profile')
+const remark = r => require.ensure([], () => r(require('../page/confirmOrder/children/remark')), 'remark')
+const payment = r => require.ensure([], () => r(require('../page/confirmOrder/children/payment')), 'payment')
+const userValidation = r => require.ensure([], () => r(require('../page/confirmOrder/children/userValidation')), 'userValidation')
+const invoice = r => require.ensure([], () => r(require('../page/confirmOrder/children/invoice')), 'invoice')
+const chooseAddress = r => require.ensure([], () => r(require('../page/confirmOrder/children/chooseAddress')), 'chooseAddress')
+const addAddress = r => require.ensure([], () => r(require('../page/confirmOrder/children/children/addAddress')), 'addAddress')
+const searchAddress = r => require.ensure([], () => r(require('../page/confirmOrder/children/children/children/searchAddress')), 'searchAddress')
 
 const foodDetail = r => require.ensure([], () => r(require('../page/shop/children/foodDetail')), 'foodDetail')
 const shopDetail = r => require.ensure([], () => r(require('../page/shop/children/shopDetail')), 'shopDetail')
 const confirmOrder = r => require.ensure([], () => r(require('../page/confirmOrder/confirmOrder')), 'confirmOrder')
 const shopSafe = r => require.ensure([], () => r(require('../page/shop/children/children/shopSafe')), 'shopSafe')
 const download = r => require.ensure([], () => r(require('../page/download/download')), 'download')
-// const info = r => require.ensure([], () => r(require('../page/profile/children/info')), 'info')
-// const setusername = r => require.ensure([], () => r(require('../page/profile/children/children/setusername')), 'setusername')
-// const address = r => require.ensure([], () => r(require('../page/profile/children/children/address')), 'address')
-// const add = r => require.ensure([], () => r(require('../page/profile/children/children/children/add')), 'add')
-// const addDetail = r => require.ensure([], () => r(require('../page/profile/children/children/children/children/addDetail')), 'addDetail')
+const info = r => require.ensure([], () => r(require('../page/profile/children/info')), 'info')
+const setusername = r => require.ensure([], () => r(require('../page/profile/children/children/setusername')), 'setusername')
+const address = r => require.ensure([], () => r(require('../page/profile/children/children/address')), 'address')
+const add = r => require.ensure([], () => r(require('../page/profile/children/children/children/add')), 'add')
+const addDetail = r => require.ensure([], () => r(require('../page/profile/children/children/children/children/addDetail')), 'addDetail')
+const service = r => require.ensure([], () => r(require('../page/service/service')), 'service')
+const questionDetail = r => require.ensure([], () => r(require('../page/service/children/questionDetail')), 'questionDetail')
+
+const benefit = r => require.ensure([], () => r(require('../page/benefit/benefit')), 'benefit')
+const coupon = r => require.ensure([], () => r(require('../page/benefit/children/coupon')), 'coupon')
+const hbDescription = r => require.ensure([], () => r(require('../page/benefit/children/hbDescription')), 'hbDescription')
+const hbHistory = r => require.ensure([], () => r(require('../page/benefit/children/hbHistory')), 'hbHistory')
+const exchange = r => require.ensure([], () => r(require('../page/benefit/children/exchange')), 'exchange')
+const commend = r => require.ensure([], () => r(require('../page/benefit/children/commend')), 'commend')
 
 
 export default [{
@@ -47,30 +63,29 @@ export default [{
         {
             path: '/profile',
             component: profile,
-            // children: [{
-            //     path: 'info', //个人信息详情页
-            //     component: info,
-            //     children: [{
-            //         path: 'setusername',
-            //         component: setusername,
-            //     },{
-            //         path: 'address',
-            //         component: address,     //编辑地址
-            //         children:[{
-            //             path:'add',
-            //             component:add,
-            //             children:[{
-            //                 path:'addDetail',
-            //                 component:addDetail
-            //             }]
-            //         }]
-            //     }]
-            // },
-            // ]
-            // {
-            //     path: 'service', //服务中心
-            //     component: service,
-            // },
+            children: [{
+                path: 'info', //个人信息详情页
+                component: info,
+                children: [{
+                    path: 'setusername',
+                    component: setusername,
+                },{
+                    path: 'address',
+                    component: address,     //编辑地址
+                    children:[{
+                        path:'add',
+                        component:add,
+                        children:[{
+                            path:'addDetail',
+                            component:addDetail
+                        }]
+                    }]
+                }]
+            },
+            {
+                path: 'service', //服务中心
+                component: service,
+            },]
         },
         // 首页
         {
@@ -98,35 +113,65 @@ export default [{
         {
             path: '/confirmOrder',
             component: confirmOrder,
-            // children: [{
-            //     path: 'remark', //订单备注
-            //     component: remark,
-            // }, {
-            //     path: 'invoice', //发票抬头
-            //     component: invoice,
-            // }, {
-            //     path: 'payment', //付款页面
-            //     component: payment,
-            // }, {
-            //     path: 'userValidation', //用户验证
-            //     component: userValidation,
-            // }, {
-            //     path: 'chooseAddress', //选择地址
-            //     component: chooseAddress,
-            //     children: [{
-            //         path: 'addAddress', //添加地址
-            //         component: addAddress,
-            //         children: [{
-            //             path: 'searchAddress', //搜索地址
-            //             component: searchAddress,
-            //         }]
-            //     }, ]
-            // }, ]
+            children: [{
+                path: 'remark', //订单备注
+                component: remark,
+            }, {
+                path: 'invoice', //发票抬头
+                component: invoice,
+            }, {
+                path: 'payment', //付款页面
+                component: payment,
+            }, {
+                path: 'userValidation', //用户验证
+                component: userValidation,
+            }, {
+                path: 'chooseAddress', //选择地址
+                component: chooseAddress,
+                children: [{
+                    path: 'addAddress', //添加地址
+                    component: addAddress,
+                    children: [{
+                        path: 'searchAddress', //搜索地址
+                        component: searchAddress,
+                    }]
+                }, ]
+            }, ]
         },
         //下载页
         {
             path: '/download',
             component: download
+        },
+        //服务中心
+        {
+            path: '/service',
+            component: service,
+             children: [{
+                path: 'questionDetail', //订单详情页
+                component: questionDetail,
+            }, ]
+        },
+        //我的优惠页
+        {
+            path: 'benefit',
+            component: benefit,
+            children: [{
+                path: 'coupon', //代金券说明
+                component: coupon,
+            }, {
+                path: 'hbDescription', //红包说明
+                component: hbDescription,
+            }, {
+                path: 'hbHistory', //历史红包
+                component: hbHistory,
+            }, {
+                path: 'exchange', //兑换红包
+                component: exchange,
+            }, {
+                path: 'commend', //推荐有奖
+                component: commend,
+            },]
         },
     ]
 }]
